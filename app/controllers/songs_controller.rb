683d5959f95c0321ab811d26ed6df50eb352f00b
Elaine Anderson <eparie@bu.edu>
2018-07-23 21:44:30 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -35,6 +35,7 @@ enable :sessions
     patch '/songs/:slug' do
         @song = Song.find_by_slug(params[:slug])
         @song.update(params[:song])
+        @song.artist = Artist.find_or_create_by(params["Artist Name"])
         @song.genres << Genre.find_by(id: params["Genre Name"])
         @song.save
         redirect to "/songs/#{@song.slug}"
