e850c0ceaf99fbd98f7bcba1411ef9cb5bed4c68
Elaine Anderson <eparie@bu.edu>
2018-07-23 20:32:50 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -36,6 +36,8 @@ class SongsController < ApplicationController
 
     patch '/songs/:slug' do
 
+      "Successfully Updated Song"
+
       if !!Artist.find_by(name: params["Artist Name"])
         @artist = Artist.find_by(name: params["Artist Name"])
         @song = Song.update(:name => params[:name], :artist_id => @artist.id)
