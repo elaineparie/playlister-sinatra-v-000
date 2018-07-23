b88b488b3666325ea6984d26214e4394a96d26cc
Elaine Anderson <eparie@bu.edu>
2018-07-23 20:16:27 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -39,6 +39,7 @@ class SongsController < ApplicationController
       if !!Artist.find_by(name: params["Artist Name"])
         @artist = Artist.find_by(name: params["Artist Name"])
         @song = Song.update(:name => params[:name], :artist_id => @artist.id)
+        binding.pry
        @song.genres.destroy
       #  @song.genres << Genre.find_by(id: params["Genre Name"])
         redirect to "/songs/#{@song.slug}"
